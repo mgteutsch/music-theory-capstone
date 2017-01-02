923a611a7df7635374f639fc04308b9fe376adc1
Mike Teutsch <mgt0117@gmail.com>
2017-01-01 23:43:36 -0600

    git wasn't adding certain files, added missing files

--- javascripts/controllers/ImproviserCtrl.js
+++ javascripts/controllers/ImproviserCtrl.js
@@ -3,18 +3,41 @@
 app.controller("ImproviserCtrl", function($q, $scope, $rootScope, $location){
 
 	$scope.playI = function(){
+		angular.element(document.querySelectorAll('#improviserBackground')).removeClass();
+		angular.element(document.querySelectorAll('#improviserBackground')).addClass("background-I");
+		angular.element(document.querySelectorAll('a#improviserKey')).removeClass();
+		angular.element(document.querySelectorAll('a#improviserKey')).addClass("col s1 btn key-I");
+		angular.element(document.querySelectorAll('a#improviserKeyAlt')).removeClass();
+		angular.element(document.querySelectorAll('a#improviserKeyAlt')).addClass("col s1 btn keyAlt-I");
+		angular.element(document.querySelectorAll('#improviserTitle')).removeClass();
+		angular.element(document.querySelectorAll('#improviserTitle')).addClass("textColor-I");
+
 		var audioI = new Audio("audio/composerChords/I.mp3");
 		audioI.play();
 	};
 
 
 	$scope.playii = function(){
+		angular.element(document.querySelector('#improviserBackground')).removeClass();
+		angular.element(document.querySelector('#improviserBackground')).addClass("background-ii");
+		angular.element(document.querySelectorAll('#improviserBackground')).removeClass();
+		angular.element(document.querySelectorAll('#improviserBackground')).addClass("background-ii");
+		angular.element(document.querySelectorAll('a#improviserKey')).removeClass();
+		angular.element(document.querySelectorAll('a#improviserKey')).addClass("col s1 btn key-ii");
+		angular.element(document.querySelectorAll('a#improviserKeyAlt')).removeClass();
+		angular.element(document.querySelectorAll('a#improviserKeyAlt')).addClass("col s1 btn keyAlt-ii");
+		angular.element(document.querySelectorAll('#improviserTitle')).removeClass();
+		angular.element(document.querySelectorAll('#improviserTitle')).addClass("textColor-ii");
+
 		var audioii = new Audio("audio/composerChords/minor-ii.mp3");
 		audioii.play();
 	};
 
 
 	$scope.playiii = function(){
+		angular.element(document.querySelector('#improviserBackground')).removeClass();
+		angular.element(document.querySelector('#improviserBackground')).addClass("background-iii");
+
 		var audioiii = new Audio("audio/composerChords/minor-iii.mp3");
 		audioiii.play();
 	};
